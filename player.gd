f2160fe3a4134ddbdc964cf2e6a6b3790551c7f7
vindictivolence <59714557+vindictivolence@users.noreply.github.com>
2025-01-30 23:08:36 -0600

    Map changes
    - Added foreground
    - Adjusted collision boxes
    - Added more details to map

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var speed = 200
 
-func get_input():
+func get_input():	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_dir * speed
 
